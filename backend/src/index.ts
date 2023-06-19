418567f08633f1cb857eefeadaeb4ef38b480c44
pwkang <porweikang@hotmail.com>
2023-06-19 11:57:47 +0800

    chore: extends request body size

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ dotenv.config()
 const PORT = process.env.PORT || 8080
 
 const app = express()
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 app.use(
   cors({
     origin: '*',
