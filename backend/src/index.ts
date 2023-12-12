c492229fcfc5da7f814b18d5106ce8d586879b7f
Pwkang <porweikang@hotmail.com>
2023-12-12 22:52:38 +0800

    chore: update port variables (#4)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.API_PORT || 8080
 
 const app = express()
 app.use(express.json({ limit: '50mb' }))
